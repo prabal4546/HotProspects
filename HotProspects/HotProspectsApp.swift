459b4de175088b2a36091866b107298b51550020
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-02-02 19:21:50 +0530

    first commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by PRABALJIT WALIA     on 02/02/21.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
